bff545e6ae36a2a2c0bd15fb638347ca306fdcb2
PLatCIC <peter.lindes@cic.iqmri.org>
2026-04-03 09:56:40 -0600

    Edited SetAPIKey scripts to say what they have done.

--- SetAPIKey/GoCASC2.ps1
+++ SetAPIKey/GoCASC2.ps1
@@ -1,1 +1,3 @@
 [Environment]::SetEnvironmentVariable('OPENAI_API_KEY',$Env:OPENAI_CASC2_KEY,'Machine')
+echo "OPENAI_API_KEY is now set for the CASC2 project."
+Pause
--- SetAPIKey/GoThorSoar.ps1
+++ SetAPIKey/GoThorSoar.ps1
@@ -1,1 +1,4 @@
 [Environment]::SetEnvironmentVariable('OPENAI_API_KEY',$Env:OPENAI_THOR_SOAR_KEY,'Machine')
+echo "OPENAI_API_KEY is now set for the Thor-Soar project."
+Pause
+
