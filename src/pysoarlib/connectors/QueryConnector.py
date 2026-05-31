4129e10295e2b12704d730873ba32f0fa4cdd022
jrkirk <jrkirk@umich.edu>
2026-05-31 19:23:55 -0400

    templates for component level hypothesis generation

--- src/pysoarlib/connectors/QueryConnector.py
+++ src/pysoarlib/connectors/QueryConnector.py
@@ -214,6 +214,8 @@ class QueryConnector(AgentConnector):
                         argument = json.dumps(soar_identifier_to_json_limited(arg.ConvertToIdentifier(), exclusions=["argument1", "argument2", "argument3", "argument4", "node-result"], depth_limit=2), indent=4)
                     elif (request_type == "causal-discrepency-hypothesis" or request_type == "hypothesis-test-repair") and i == 1:
                         argument = json.dumps(soar_identifier_to_json_limited(arg.ConvertToIdentifier(), exclusions=[], depth_limit=2), indent=4)
+                    elif (request_type == "component-hypothesis") and i == 1:
+                        argument = json.dumps(soar_identifier_to_json_limited(arg.ConvertToIdentifier(), exclusions=["intra-connection", "outgoing-connection", "incoming-connection"], depth_limit=2), indent=4)
                     else:
                         argument = json.dumps(soar_identifier_to_json(arg.ConvertToIdentifier()), indent=4)
             arguments.append(argument)
