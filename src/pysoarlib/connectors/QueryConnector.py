5fb19ec8ab5a880f0fcc466c86f19cab425959ff
jrkirk <jrkirk@umich.edu>
2026-09-11 14:25:20 -0400

    modifications for HLG translation for events with predicted data

--- src/pysoarlib/connectors/QueryConnector.py
+++ src/pysoarlib/connectors/QueryConnector.py
@@ -211,7 +211,7 @@ class QueryConnector(AgentConnector):
                     """
                     # argument = arg.ConvertToIdentifier()
                     #hack for now, make option in config to specify exclusions and depth limit
-                    if request_type == "translate-hlg-result" and i == 3:
+                    if (request_type == "translate-hlg-result" or request_type == "translate-hlg-event-detection") and i == 3:
                         argument = json.dumps(soar_identifier_to_json_limited(arg.ConvertToIdentifier(), exclusions=["argument1", "argument2", "argument3", "argument4", "node-result"], depth_limit=2), indent=4)
                     else:
                         argument = json.dumps(soar_identifier_to_json(arg.ConvertToIdentifier()), indent=4)
